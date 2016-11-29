de82028d4da45c10d55bb4d65a5627bdc6de1df4
Marouan Bakour <marouano@FreeThinker.local>
2016-11-29 13:26:24 +0100

    comm

---
+++ PTS3-ServerSide/src/server/IComms.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.rmi.Remote;
+
+/**
+ *
+ * @author Milton van de Sanden
+ */
+public interface IComms extends Remote
+{
+}
--- PTS3-ServerSide/src/server/ServerComm.java
+++ PTS3-ServerSide/src/server/ServerComm.java
@@ -5,21 +5,57 @@
  */
 package server;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.awt.Point;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+import server.player.Player;
+
 /**
  *
  * @author marouano
  */
-public class ServerComm {
-    public Lobby Login(String username){
+public class ServerComm extends UnicastRemoteObject implements IServerComm {
+    public static final String MESSAGE = "Connected with server yo!";
+    
+    public ServerComm() throws RemoteException{
+        super(0);
+        
+    }
+    
+    @Override
+    public String getMessage() {
+        return MESSAGE;
+    }
+    
+    @Override
+    public List<Player> Login(String username){
         throw new UnsupportedOperationException();
     }
     
+    @Override
     public boolean roleSwitch(String username){
         throw new UnsupportedOperationException();
     }
     
+    @Override
     public void ready(String username){
         throw new UnsupportedOperationException();
     }
+    
+    @Override
+    public void mapSwitch(String mapName){
+        throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public void pushPos(String username, Point pos){
+        throw new UnsupportedOperationException();
+    }
+
     
 }
