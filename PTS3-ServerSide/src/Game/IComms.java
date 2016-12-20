3eaecc7ccf915d1755d3e66b6a422a97dce7d0cb
Milton van de Sanden <milton.vandesanden@gmail.com>
2016-12-20 11:46:37 +0100

    added receiveMessage to IComm

--- PTS3-ServerSide/src/Game/IComms.java
+++ PTS3-ServerSide/src/Game/IComms.java
@@ -5,6 +5,7 @@
  */
 package Game;
 
+import Chat.Chatmessage;
 import java.awt.Point;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -16,4 +17,5 @@ import java.rmi.RemoteException;
 public interface IComms extends Remote
 {
     public void pushPlayerPosition(String username, Point position, float rotation) throws RemoteException;
+    public void receiveNewChatmessage(Chatmessage chatmessage) throws RemoteException;
 }
